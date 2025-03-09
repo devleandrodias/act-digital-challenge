2e8fdc2bd8cbdd91db3d2e9272e4ecfba73b17d1
devleandrodias <leandrodbdias@gmail.com>
2025-03-09 13:01:48 -0300

    feat: implemented queries for get data to dashboard

--- backend/src/modules/dashboard/dashboard.module.ts
+++ backend/src/modules/dashboard/dashboard.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
 
+import { Farm } from '@modules/farm/farm.entity';
+import { Crop } from '@modules/crop/crop.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Farm, Crop])],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
--- backend/src/modules/dashboard/dashboard.service.ts
+++ backend/src/modules/dashboard/dashboard.service.ts
@@ -1,12 +1,76 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Crop } from '@modules/crop/crop.entity';
+import { Farm } from '@modules/farm/farm.entity';
 
 @Injectable()
 export class DashboardService {
-  getDashboardData() {
+  constructor(
+    @InjectRepository(Farm)
+    private farmRepository: Repository<Farm>,
+    @InjectRepository(Crop)
+    private cropRepository: Repository<Crop>,
+  ) {}
+
+  async getDashboardData() {
+    const totalFarms = await this.farmRepository.count();
+    const totalCrops = await this.cropRepository.count();
+
+    const totalAreaData = await this.farmRepository
+      .createQueryBuilder('farm')
+      .select('SUM(farm.totalArea)', 'totalArea')
+      .addSelect('SUM(farm.agriculturalArea)', 'agriculturalArea')
+      .addSelect('SUM(farm.vegetationArea)', 'vegetationArea')
+      .getRawOne<{
+        totalArea: number;
+        agriculturalArea: number;
+        vegetationArea: number;
+      }>();
+
+    const farmsByState = await this.farmRepository
+      .createQueryBuilder('farm')
+      .select('farm.state', 'name')
+      .addSelect('CAST(COUNT(*) AS INTEGER)', 'value')
+      .groupBy('farm.state')
+      .getRawMany<{
+        name: string;
+        value: number;
+      }>();
+
+    const cropsByType = await this.cropRepository
+      .createQueryBuilder('crop')
+      .select('crop.name', 'name')
+      .addSelect('CAST(COUNT(*) AS INTEGER)', 'value')
+      .groupBy('crop.name')
+      .getRawMany<{
+        name: string;
+        value: number;
+      }>();
+
+    const totalArea = Number(totalAreaData?.totalArea || 0);
+    const agriculturalArea = Number(totalAreaData?.agriculturalArea || 0);
+    const vegetationArea = Number(totalAreaData?.vegetationArea || 0);
+    const unidentifiedArea = Number(
+      totalArea - (agriculturalArea + vegetationArea),
+    );
+
     return {
-      totalFarms: 20,
-      totalArea: 653,
-      totalCrops: 4,
+      totalFarms,
+      totalArea,
+      totalCrops,
+      agriculturalArea,
+      vegetationArea,
+      unidentifiedArea: unidentifiedArea > 0 ? unidentifiedArea : 0,
+      farmsByState: farmsByState.map((state) => ({
+        name: state.name,
+        value: Number(state.value),
+      })),
+      cropsByType: cropsByType.map((crop) => ({
+        name: crop.name,
+        value: Number(crop.value),
+      })),
     };
   }
 }
